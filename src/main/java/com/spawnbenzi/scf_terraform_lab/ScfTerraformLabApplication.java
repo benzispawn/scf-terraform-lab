7e8247096f3a055c10c2fdb4140cd6202919413e
benzispawn <spawnbenzi@gmail.com>
2025-12-21 03:33:05 -0300

    Init project

---
+++ src/main/java/com/spawnbenzi/scf_terraform_lab/ScfTerraformLabApplication.java
@@ -0,0 +1,13 @@
+package com.spawnbenzi.scf_terraform_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScfTerraformLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScfTerraformLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spawnbenzi/scf_terraform_lab/ScfTerraformLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spawnbenzi.scf_terraform_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScfTerraformLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
